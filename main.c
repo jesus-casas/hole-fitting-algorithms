3fa1d3f8257605d2d6ae6e87c7d44b927d88de66
jesus-casas <javiercasas740@gmail.com>
2024-04-14 19:55:55 -0700

    added print statement for userinput

--- main.c
+++ main.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 
 int main() {
-    printf(" Test main\n");
+    int choice;
+    
+    while (1)
+    {
+        printf("\nHole-fitting Algorithms\n");
+        printf("-----------------------\n");
+        printf("1) Enter parameters\n");
+        printf("2) Allocate memory for block using First-fit\n");
+        printf("3) Allocate memory for block using Best-fit\n");
+        printf("4) Deallocate memory for block\n");
+        printf("5) Defragment memory\n");
+        printf("6) Quit program\n");
+        printf("\nEnter selection: ");
+        // Ask user for input
+        scanf("%d", &choice);
+
+        // Add switch case to handle user input
+
+    } // end while loop
+    
     return 0;
 }
