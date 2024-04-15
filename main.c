f574988ddfa8942f53818da5714121d6fc504916
jesus-casas <javiercasas740@gmail.com>
2024-04-14 20:13:45 -0700

    added switch for user inputs and added enter_parameters function with the struct memory_block

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+    // Define structure for memory block
+    struct memory_block {
+        int block_id;
+        int block_size;
+        int start_address;
+        int end_address;
+        int is_allocated;
+        struct memory_block *next;
+    } memory_block;
+
+    // Declare global variables
+    int physical_memory_size;
+    
+
+    // Function declarations
+    void enter_parameters();
+    void allocate_memory(int);
+    void deallocate_memory();
+    void defragment_memory();
+    void print_blocks();
+    void free_all_memory();
+
+    // Enter parameters function
+    void enter_parameters() {
+    printf("Enter size of physical memory: ");
+    scanf("%d", &physical_memory_size);
+}
+
 int main() {
     int choice;
     
@@ -19,7 +47,32 @@ int main() {
         scanf("%d", &choice);
 
         // Add switch case to handle user input
+        switch (choice) {
+            case 1:
+                enter_parameters();
+                break;
+            case 2:
+                allocate_memory(1); // First-fit
+                break;
+            case 3:
+                allocate_memory(2); // Best-fit
+                break;
+            case 4:
+                deallocate_memory();
+                break;
+            case 5:
+                defragment_memory();
+                break;
+            case 6:
+                printf("Quitting program...\n");
+                // Implement free all memory blocks
 
+                return 0;
+            default:
+                printf("Invalid choice. Please enter a number between 1 and 6.\n");
+        }
+        // Print blocks after each operation
+    print_blocks();
     } // end while loop
     
     return 0;
